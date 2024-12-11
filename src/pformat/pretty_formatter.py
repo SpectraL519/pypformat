f5e8715651d9f143a98deb12463e3c69923ec833
SpectraL519 <jmusial2003@gmail.com>
2024-12-11 11:32:27 +0100

    initial PrettyFormatter logic - return str(obj)

--- src/pformat/pretty_formatter.py
+++ src/pformat/pretty_formatter.py
@@ -1,2 +1,9 @@
 class PrettyFormatter:
-    pass
+    def __init__(self):
+        pass
+
+    def __call__(self, obj, depth: int = 0):
+        return str(obj)
+
+    def format(self, obj, depth: int = 0) -> str:
+        return self(obj, depth)
--- test/test_pretty_formatter.py
+++ test/test_pretty_formatter.py
@@ -1,5 +1,13 @@
+import pytest
+
 from pformat.pretty_formatter import PrettyFormatter
 
+from icecream import ic
 
-def test_dummy():
-    assert isinstance(PrettyFormatter(), PrettyFormatter)
+
+@pytest.mark.parametrize("data", ["string", 123, 3.14, b"bytes"])
+def test_format_simple(data):
+    sut = PrettyFormatter()
+
+    assert sut(data) == str(data)
+    assert sut.format(data) == str(data)
