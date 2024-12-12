28ab991cec6b9576d6041bb14e128863223bccbc
SpectraL519 <jmusial2003@gmail.com>
2024-12-12 21:49:40 +0100

    ...

--- README.md
+++ README.md
@@ -4,9 +4,3 @@ Python pretty formatting package
 
 [![tests](https://github.com/SpectraL519/pypformat/actions/workflows/tests.yaml/badge.svg)](https://github.com/SpectraL519/pypformat/actions/workflows/tests)
 [![ruff - linter & formatter](https://github.com/SpectraL519/pypformat/actions/workflows/ruff.yaml/badge.svg)](https://github.com/SpectraL519/pypformat/actions/workflows/ruff)
-
-<br />
-
-## TODO
-
-- Remove the `icecream` dependency from the `test.yaml` workflow before release
--- test/test_format_options.py
+++ test/test_format_options.py
@@ -1,3 +1,5 @@
+from dataclasses import field
+
 from pformat.format_options import FormatOptions
 
 
@@ -5,3 +7,8 @@ def test_default():
     assert FormatOptions.default("width") == 80
     assert FormatOptions.default("indent_width") == 4
     assert FormatOptions.default("compact") == False
+
+    # Dummy field with a default factory
+    list_field = "list"
+    FormatOptions.__dataclass_fields__[list_field] = field(default_factory=list)
+    assert FormatOptions.default(list_field) == list()
